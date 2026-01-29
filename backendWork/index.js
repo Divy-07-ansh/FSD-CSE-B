e6296ff3117541fc147f4ad60d8d872130482b77
Divyansh Verma <divyanshverma812@gmail.com>
2026-01-29 09:19:30 +0530

    backend

--- backendWork/index.js
+++ backendWork/index.js
@@ -1,1 +1,11 @@
 const http= require("http")
+const PORT=4005;
+
+const server=http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.end("<h2>hiii...</h2>");
+})
+
+server.listen(PORT,()=>{
+    console.log('Server is running on ${PORT}')
+})
