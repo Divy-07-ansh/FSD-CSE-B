a572b40f0c75caf942ba5fd5e2c7720a83f63934
Divyansh Verma <divyanshverma812@gmail.com>
2026-01-29 09:34:12 +0530

    post

--- backendWork/index.js
+++ backendWork/index.js
@@ -2,8 +2,17 @@ const http= require("http")
 const PORT=4005;
 
 const server=http.createServer((req,res)=>{
-    res.setHeader('Content-Type','text/html');
-    res.end("<h2>hiii...</h2>");
+    // res.setHeader('Content-Type','text/html');
+    // res.end("<h2>hiii...</h2>");
+    if(req.url=="/msg" && req.method=="GET"){
+        res.setHeader('Content-Type','text/html');
+        res.end('<h2 style=color:red>Welcome to Server</h2>')
+    }
+    if(req.url=="/data" && req.method=="POST"){
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify({msg:"Data inserted successfully"}));
+    }
+    // console,log(Object.keys(req))
 })
 
 server.listen(PORT,()=>{
