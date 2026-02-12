aa33c56e3002d2380d6becd3fd0027fcb6538b5a
Divyansh Verma <148184991+Divy-07-ansh@users.noreply.github.com>
2026-02-12 09:29:30 +0530

    Update App.jsx

--- newWork/frontend/src/App.jsx
+++ newWork/frontend/src/App.jsx
@@ -4,19 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const[data,setData]=useState([]);
-  async function getFetchData(){
-     const serverData=await fetch('http://localhost:4002/data');
-          const jsonData=await serverData.json();
-          setData(jsonData.msg);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  async function getFetchData() {
+    try{
+      setLoading(true);
+      const serverData = await fetch('http://localhost:4002/data');
+      const jsonData = await serverData.json();
+      setData(jsonData.msg);
+    }catch(e){
+      console.log(e);
+    }
+    finally{
+      setLoading(false);
+    }
+    
 
   }
 
   return (
     <>
-     <h2>Welcome to Node Fetch API</h2> 
-     {JSON.stringify(data)}
-     <button onClick={getFetchData}>fetchdata</button>
+      <h2>Welcome to Node Fetch API</h2>
+      {
+        data.map((ele) => (
+          <div>
+            <img src={ele.image} height={100} width={200}/>
+            <h2>{ele.id}:{ele.title}</h2>
+            <h3>{ele.price}</h3>
+            
+          </div>
+        ))
+      }
+      {
+        !loading?(<></>):(<h2>Data is loading...</h2>)
+      }
+      {JSON.stringify(data)}
+      <button onClick={getFetchData}>fetchdata</button>
     </>
   )
 }
